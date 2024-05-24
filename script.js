e0f1ee1537c8c9a67d016e42756e07d53c86576c
Zach Poba-Nzaou <168301948+plectus24@users.noreply.github.com>
2024-05-23 21:42:32 -0400

    Add back and . buttons w/ validation for buttons
    Added backspace button with implementation
    
    Decimal button without implementation
    
    Completed different checks for order of button pressed to simplify
    user experience

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
 let firstNumber, secondNumber, operator;
 let inputDisplay = document.querySelector(".input");
+// allows keyboard edits on div
+inputDisplay.contentEditable = true;
 let resultDisplay = document.querySelector(".result");
 const clearButton = document.querySelector(".clear");
 const equalButton = document.querySelector(".equal");
+const backButton = document.querySelector(".back");
+const floatButton = document.querySelector(".float");
+// Regex for operation
+const REGEX = /^(\d+)([+\-*/])(\d+)$/;
+//Regex for double operands
+var REGEX2 = /^[^+\-*/]*(?:[+\-*/][^+\-*/]*)?[+\-*/][^+\-*/]*$/;
 
 function add(num1, num2) {
   return num1 + num2;
@@ -17,6 +25,9 @@ function multiply(num1, num2) {
 }
 
 function divide(num1, num2) {
+  if (num2 === 0) {
+    return "REALLY?";
+  }
   return num1 / num2;
 }
 
@@ -33,7 +44,7 @@ function operate(firstNumber, secondNumber, operator) {
       return divide(firstNumber, secondNumber);
 
     default:
-      return "OPERATOR NOT FOUND";
+      return "WRONG OPERATION";
   }
 }
 
@@ -43,6 +54,47 @@ const itemButtons = [...document.querySelectorAll(".item")];
 // Append number or operand to display on button click
 itemButtons.forEach((item) => {
   item.addEventListener("click", (e) => {
+    //check if result display is not empty --START IF
+    //if it is not, check if button pressed is numbers or operand --START IF
+    // if its operand, add the curent result as the first argument in display, then the operand clicked --END IF
+    // else if its a number, simply add the number to the input field --ELSE IF
+    //                                                                --END ELSE IF
+
+    //if result display is empty --ELSE
+    //just append clicked button to input field
+    if (
+      resultDisplay.textContent !== "" &&
+      resultDisplay !== "WRONG OPERATION" &&
+      resultDisplay !== "REALLY?"
+    ) {
+      if (e.target.className === "item number") {
+        console.log("number pressed");
+        resultDisplay.textContent = "";
+      } else {
+        console.log("operand pressed");
+        inputDisplay.textContent =
+          resultDisplay.textContent + inputDisplay.textContent;
+        resultDisplay.textContent = "";
+      }
+    }
+
+    // if an operand is pressed when there is already an operand
+    // Ignore the additional button press
+    if (
+      e.target.className === "item operand" &&
+      inputDisplay.textContent.match(REGEX2)
+    ) {
+      e.preventDefault();
+      return false;
+    }
+
+    // if (
+    //   e.target.className === "item operand" &&
+    //   resultDisplay.textContent === "WRONG OPERATION"
+    // ) {
+    //   console.log("swderfgwe");
+    // }
+
     console.log(e.target.textContent);
     inputDisplay.textContent += e.target.textContent;
   });
@@ -52,8 +104,8 @@ itemButtons.forEach((item) => {
 function parseInput(text) {
   console.log(text);
   //regular expression to identify operator
-  const regex = /^(\d+)([+\-*/])(\d+)$/;
-  const match = text.match(regex);
+
+  const match = text.match(REGEX);
   // match[1] = number1, match[2] = operator, match[3] = number2
   if (match) {
     return match;
@@ -77,5 +129,27 @@ equalButton.addEventListener("click", (e) => {
     parseInt(operation[3]),
     operation[2]
   );
-  return (resultDisplay.textContent = result);
+  // if result is good
+  if (result !== "WRONG OPERATION") {
+    inputDisplay.textContent = "";
+    return (resultDisplay.textContent = result);
+  }
+
+  // else if(result === "WRONG OPERATION" || result === "REALLY?"){
+
+  // }
+  else {
+    return (resultDisplay.textContent = result);
+  }
+  //else if result is bad
+});
+
+//back button
+backButton.addEventListener("click", (e) => {
+  inputDisplay.textContent = inputDisplay.textContent.slice(0, -1);
+});
+
+//float button
+floatButton.addEventListener("click", (e) => {
+  inputDisplay.textContent += ".";
 });
